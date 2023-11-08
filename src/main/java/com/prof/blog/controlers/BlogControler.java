908a0afa833a10b84d83e6bdedc431b129674fd2
Leonardo <leonardo20prof@gmail.com>
2023-11-08 21:20:51 +0200

    new finish comit

--- src/main/java/com/prof/blog/controlers/BlogControler.java
+++ src/main/java/com/prof/blog/controlers/BlogControler.java
@@ -49,6 +49,32 @@ public class BlogControler {
         model.addAttribute("post",res);
         return "blog-detalis";
     }
+    @GetMapping("/blog/{id}/edit")
+    public  String blogEdit(@PathVariable(value = "id") long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post>post= postRepository.findById(id);
+        ArrayList<Post>res=new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post",res);
+        return "blog-edit";
+    }
+    @PostMapping("/blog/{id}/edit")
+    public  String blockPostUpdate(@PathVariable(value = "id") long id,@RequestParam String title,@RequestParam String anons,@RequestParam String full_text, Model model){
+       Post post=postRepository.findById(id).orElseThrow();
+       post.setTitle(title);
+       post.setAnonse(anons);
+       post.setFull_text(full_text);
+       postRepository.save(post);
+        return "redirect:/blog";
+    }
+    @PostMapping("/blog/{id}/remove")
+    public  String blockPostDelete(@PathVariable(value = "id") long id, Model model){
+        Post post=postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+        return "redirect:/blog";
+    }
 
 
 }
